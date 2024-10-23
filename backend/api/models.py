1174b81a3e5d23a524b582aecbae5d8e9ea5af16
Maciej Smukowski <msmukowski@gmail.com>
2024-10-23 17:38:28 +0200

    feat(backend): initial api view

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
+import random
+import string
 from django.db import models
 
-# Create your models here.
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Room.objects.filter(code=code).count() == 0:
+            break
+
+    return code
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_stop = models.BooleanField(null=False, default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
